ce43cda04e0516f0e3504fbb0b882e0f08f248c0
AlthafKunnathPmna <althafathif100@gmail.com>
2026-02-01 19:19:49 +0530

    removed .env

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os
 
-#local 
-from dotenv import load_dotenv
+# #local 
+# from dotenv import load_dotenv
 
-# Load .env file
-load_dotenv()
+# # Load .env file
+# load_dotenv()
 
-# Read key
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+# # Read key
+# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-if not GEMINI_API_KEY:
-    raise ValueError("GEMINI_API_KEY not found in .env file")
+# if not GEMINI_API_KEY:
+#     raise ValueError("GEMINI_API_KEY not found in .env file")
 
 #Streamlit api 
 
 # Gemini API Key (set in Streamlit secrets or env var)
-# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 TEMP_DIR = "temp"
 FRAMES_DIR = f"{TEMP_DIR}/frames"
